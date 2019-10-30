cbd864a2f8553f9c49eee7317ffbb41404d5c041
Shintaro <shin.8823@outlook.jp>
2019-10-31 02:41:30 +0900

    進捗

---
+++ Neudio.Core/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neudio.Core
+{
+    class Player
+    {
+    }
+}
---
+++ Neudio.Core/WaveData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neudio.Core
+{
+    public class WaveData
+    {
+        public double[] DataArray { get; }
+        public long Length { get; }
+        public long Position { get; private set; } = 0;
+
+        public WaveData(long length)
+        {
+            Length = length;
+            DataArray = new double[Length];
+        }
+
+        public void Append(double[] datas)
+        {
+            var appendSize = Math.Min(DataArray.Length - Position - 1, datas.Length);
+            for(long i = 0;i < appendSize;i++)
+            {
+                DataArray[Position + i] = datas[i];
+            }
+
+            Position += appendSize;
+        }
+    }
+}
